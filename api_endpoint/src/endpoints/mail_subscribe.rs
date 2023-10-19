55927a6b4ccb3780248b040351015b006bb1b3c8
Thomas Marchand <thomas.marchand@tuta.io>
2023-10-19 09:24:59 +0700

    feat: support multiple groups

--- api_endpoint/src/endpoints/mail_subscribe.rs
+++ api_endpoint/src/endpoints/mail_subscribe.rs
@@ -9,8 +9,14 @@ use reqwest::StatusCode;
 use serde_derive::{Deserialize, Serialize};
 use starknet::core::types::FieldElement;
 
-#[derive(Serialize, Deserialize)]
-pub struct MailSubscribe {
+#[derive(Deserialize)]
+pub struct MailSubscribeQuery {
+    tx_hash: FieldElement,
+    groups: Vec<String>,
+}
+
+#[derive(Serialize)]
+pub struct MailSubscribeDoc {
     #[serde(serialize_with = "field_element_to_hex")]
     tx_hash: FieldElement,
     group: String,
@@ -30,21 +36,27 @@ pub struct Output {
 
 pub async fn handler(
     State(state): State<Arc<AppState>>,
-    Json(query): Json<MailSubscribe>,
+    Json(query): Json<MailSubscribeQuery>,
 ) -> impl IntoResponse {
     let emails_collection = state
         .db
         .collection::<mongodb::bson::Document>("email_groups");
 
-    let bson_doc = mongodb::bson::to_bson(&query).expect("Failed to serialize to BSON");
+    for group in query.groups {
+        let bson_doc = mongodb::bson::to_bson(&MailSubscribeDoc {
+            tx_hash: query.tx_hash,
+            group,
+        })
+        .expect("Failed to serialize to BSON");
 
-    if let mongodb::bson::Bson::Document(document) = bson_doc {
-        match emails_collection.insert_one(document, None).await {
-            Ok(_) => (),
-            Err(err) => return get_error(format!("Failed to insert document: {}", err)),
+        if let mongodb::bson::Bson::Document(document) = bson_doc {
+            match emails_collection.insert_one(document, None).await {
+                Ok(_) => (),
+                Err(err) => return get_error(format!("Failed to insert document: {}", err)),
+            }
+        } else {
+            return get_error("Failed to create BSON document".to_string());
         }
-    } else {
-        return get_error("Failed to create BSON document".to_string());
     }
 
     (StatusCode::OK, Json(Output { success: true })).into_response()
